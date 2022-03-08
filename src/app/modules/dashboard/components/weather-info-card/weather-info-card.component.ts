cc3220558616559f95e8d6cc0cf836ec7859864d
Carmelo <melonemark8@gmail.com>
2022-03-09 00:23:01 +0100

    Setup Dashboard page

---
+++ src/app/modules/dashboard/components/weather-info-card/weather-info-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-info-card',
+  templateUrl: './weather-info-card.component.html',
+  styleUrls: ['./weather-info-card.component.scss']
+})
+export class WeatherInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { WeatherInfoCardComponent } from './components/weather-info-card/weather-info-card.component';
+import { SportCardComponent } from './components/sport-card/sport-card.component';
 
 
 
 @NgModule({
   declarations: [
-    DashboardPageComponent
+    DashboardPageComponent,
+    WeatherInfoCardComponent,
+    SportCardComponent
   ],
   imports: [
     CommonModule,
