8be4028e824f4cf0165aa05903c5a69d7ba0eb50
Carmelo <melonemark8@gmail.com>
2022-03-08 23:46:00 +0100

    Complete page and responsive

--- src/app/modules/homepage/homepage.module.ts
+++ src/app/modules/homepage/homepage.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { HomepageRoutingModule } from './homepage-routing.module';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
 
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    WeatherCardComponent
   ],
   imports: [
     CommonModule,
