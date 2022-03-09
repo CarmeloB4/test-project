f9d2337ac107bd1746fb929f6951b129f66f055a
Carmelo Bastillo <66789080+CarmeloB4@users.noreply.github.com>
2022-03-09 23:23:44 +0100

    Install material and create module fo material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {EffectsModule} from "@ngrx/effects";
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import {ReactiveFormsModule} from "@angular/forms";
     StoreModule.forRoot({ weather: weatherReducer }),
     StoreDevtoolsModule.instrument({ maxAge: 25 }),
     EffectsModule.forRoot([WeatherEffects]),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/modules/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+
+export const MODULES = [];
+
+@NgModule({
+  declarations: [],
+  imports: [...MODULES],
+  exports: [...MODULES],
+})
+export class MaterialModule {}
